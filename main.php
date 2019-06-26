9b5a9dc5aa3ecf376f32a349bd3b730430d89ebc
IshanArora21 <34289247+IshanArora21@users.noreply.github.com>
2019-06-25 23:13:13 -0400

    get the styles

--- main.php
+++ main.php
@@ -5,6 +5,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" type="text/css" href="main.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> SOL </title>
     <h1 class="headerDescrip"> Senior Online Literacy </h1>
